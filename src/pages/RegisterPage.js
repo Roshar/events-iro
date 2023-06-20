212e265743451525716a74d0b98205aa288d85fe
Roshar <rashid.batukaev@gmail.com>
2023-06-20 09:48:32 +0300

    sdd

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -2,13 +2,14 @@ import Header from "../components/header/Header";
 import Registration from "../components/registration/Registration";
 
 const RegisnterPage = () => {
-    return (
-        <>
-            <Header />
-            <main className="main">
-                <Registration />
-            </main>
-        </>);
-}
+  return (
+    <>
+      <Header />
+      <main className="main">
+        <Registration />
+      </main>
+    </>
+  );
+};
 
 export default RegisnterPage;
