259a47088f4a7f513f0b097277ca582d34799959
roshar <rashid.batukaev@.com>
2023-07-13 16:05:29 +0300

    validate

--- src/components/registration/Registration.js
+++ src/components/registration/Registration.js
@@ -24,35 +24,131 @@ const Registration = ({ id, title }) => {
     })
 
     const [result, setResult] = useState({});
-
-
+    const [messageNot, setMessageNot] = useState('');
 
     const handleChange = (e) => {
         setEvent(prev => ({ ...prev, [e.target.name]: e.target.value }))
 
     }
 
-    const checkForm = () => {
-        const name_i = document.getElementById("name_i").value;
-        const surname_i = document.getElementById("name_i").value;
-        const patronymic_i = document.getElementById("name_i").value;
+    const name_i = document.getElementById("name_i");
+    const surname_i = document.getElementById("surname_i");
+    const email = document.getElementById("email_i");
+    const phone_i = document.getElementById("phone_i");
+    const company_i = document.getElementById("company_i");
+    const position = document.getElementById("position");
+    const area_id = document.getElementById("area_id");
+
+
+    const checkError = () => {
+        return document.querySelectorAll('.error');
+    }
+
+    const setError = (el, msg) => {
+        const inputControl = el.parentElement;
+        const errorDisplay = inputControl.querySelector('.notif');
+
+        errorDisplay.innerText = msg;
+        inputControl.classList.add('error');
+        inputControl.classList.remove('success')
+
+    }
+
+    const setSuccess = el => {
+        const inputControl = el.parentElement;
+        const errorDisplay = inputControl.querySelector('.notif');
+
+        errorDisplay.innerText = '';
+        inputControl.classList.add('success');
+        inputControl.classList.remove('error')
+    }
+
+    const isValidEmail = email => {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    const validateInputs = () => {
+        const nameVal = name_i.value.trim();
+        const surnameVal = surname_i.value.trim();
+        const emailVal = email.value.trim();
+        const phoneVal = phone_i.value.trim();
+        const companyVal = company_i.value.trim();
+        const positionVal = position.value.trim();
+        const areaSelectVal = area_id.value;
+
+        if (nameVal === '') {
+            setError(name_i, 'Поле необходимо заполнить')
+        } else {
+            setSuccess(name_i)
+        }
+
+        if (surnameVal === '') {
+            setError(surname_i, 'Поле необходимо заполнить')
+        } else {
+            setSuccess(surname_i)
+        }
+
+        if (emailVal === '') {
+            setError(email, 'Поле необходимо заполнить');
+        } else if (!isValidEmail(emailVal)) {
+            setError(email, 'Невалидный почтовый адрес');
+        } else {
+            setSuccess(email);
+        }
+
+        if (phoneVal === '') {
+            setError(phone_i, 'Поле необходимо заполнить')
+        } else {
+            setSuccess(phone_i)
+        }
+
+        if (positionVal === '') {
+            setError(position, 'Поле необходимо заполнить')
+        } else {
+            setSuccess(position)
+        }
+
+        if (companyVal === '') {
+            setError(company_i, 'Поле необходимо заполнить')
+        } else {
+            setSuccess(company_i)
+        }
+
+        if (areaSelectVal === '') {
+            setError(area_id, 'Необходимо выбрать город/район')
+        } else {
+            setSuccess(area_id)
+        }
 
 
     }
 
-    const onClick = async (e) => {
-        checkForm()
-        e.preventDefault()
 
-        try {
-            const resObj = await axios.post(`${process.env.REACT_APP_BASE_URL}/register`, event)
-            resObj.event_id = id;
-            setEvent(resObj);
-            navigate(`/registered/${resObj.data.user_id_link}`)
-            localStorage.setItem(resObj.data.user_id_link, JSON.stringify(resObj))
-        } catch (err) {
-            console.log(err.message)
+    const onClick = async (e) => {
+        e.preventDefault()
+        validateInputs()
+        const errorCount = checkError()
+
+        if (errorCount.length < 1) {
+            try {
+                const resObj = await axios.post(`${process.env.REACT_APP_BASE_URL}/register`, event)
+                resObj.event_id = id;
+                if (resObj.data.errorIsRow === 1) {
+                    console.log(resObj.data.msg)
+                    setMessageNot(resObj.data.msg)
+                } else {
+                    setEvent(resObj);
+                    navigate(`/registered/${resObj.data.user_id_link}`)
+                    localStorage.setItem(resObj.data.user_id_link, JSON.stringify(resObj))
+                }
+
+            } catch (err) {
+                console.log(err.message)
+            }
         }
+
+
     }
 
 
@@ -89,7 +185,7 @@ const Registration = ({ id, title }) => {
                             <span className="register__required">*</span> Фамилия: </label>
                         <input className="register__input" id="surname_i" type="text" name="surname" aria-labelledby="surname_i"
                             aria-describedby="danger-surname" aria-required="true" required onChange={handleChange} />
-                        <span className="txt-danger" id="danger-surname">Заполните поле</span>
+                        <span className="notif" id="danger-surname"></span>
                     </div>
 
 
@@ -98,7 +194,7 @@ const Registration = ({ id, title }) => {
                             <span className="register__required">*</span> Имя: </label>
                         <input className="register__input" id="name_i" type="text" name="firstname" aria-labelledby="name_i"
                             aria-describedby="danger-name" aria-required="true" required onChange={handleChange} />
-                        <span className="txt-danger" id="danger-name">Заполните поле</span>
+                        <span className="notif" id="danger-name"></span>
                     </div>
 
 
@@ -107,7 +203,7 @@ const Registration = ({ id, title }) => {
                             Отчество: </label>
                         <input className="register__input" id="patronymic_i" type="text" name="patronymic" aria-labelledby="patronymic_i"
                             aria-describedby="danger-patronymic" onChange={handleChange} />
-                        <span className="txt-danger" id="danger-patronymic">Заполните поле</span>
+                        <span className="notif" id="danger-patronymic"></span>
                     </div>
 
 
@@ -116,55 +212,58 @@ const Registration = ({ id, title }) => {
                             <span className="register__required">*</span> Email: </label>
                         <input className="register__input" id="email_i" type="email" name="email" aria-labelledby="email_i"
                             aria-describedby="danger-email" aria-required="true" required onChange={handleChange} />
-                        <span className="txt-danger" id="danger-email">Заполните поле</span>
+                        <span className="notif " id="danger-email"> </span>
                     </div>
 
 
                     <div className="register__block">
                         <label className="register__label" htmlFor="phone_i" id="phone_l" >
                             <span className="register__required">*</span>  Телефон: </label>
-                        <input className="register__input" id="email_i" type="phone" name="phone" aria-labelledby="phone_i"
+                        <input className="register__input" id="phone_i" type="phone" name="phone" aria-labelledby="phone_i"
                             aria-describedby="danger-phone" aria-required="true" required onChange={handleChange} />
-                        <span className="txt-danger" id="danger-phone">Заполните поле</span>
+                        <span className="notif" id="danger-phone"></span>
                     </div>
 
 
                     <div className="register__block">
                         <label className="register__label" htmlFor="company_i" id="company_l" >
                             <span className="register__required">*</span>  Организация: </label>
-                        <input className="register__input" id="email_i" type="text" name="company" aria-labelledby="company_i"
+                        <input className="register__input" id="company_i" type="text" name="company" aria-labelledby="company_i"
                             aria-describedby="danger-company" aria-required="true" required onChange={handleChange} />
-                        <span className="txt-danger" id="danger-company">Заполните поле</span>
+                        <span className="notif" id="danger-company"></span>
                     </div>
 
 
                     <div className="register__block">
                         <label className="register__label" htmlFor="position_i" id="position_l" >
                             <span className="register__required">*</span>  Должность: </label>
-                        <input className="register__input" id="position_i" type="text" name="position" aria-labelledby="position_i"
+                        <input className="register__input" id="position" type="text" name="position" aria-labelledby="position_i"
                             aria-describedby="danger-position" aria-required="true" required onChange={handleChange} />
-                        <span className="txt-danger" id="danger-position">Заполните поле</span>
+                        <span className="notif" id="danger-position">Заполните поле</span>
                     </div>
 
 
                     <div className="register__block">
-                        <label className="register__label" htmlFor="position_i" id="position_l" >
+                        <label className="register__label" htmlFor="area_id" id="area_id_l" >
                             <span className="register__required">*</span> Город/район: </label>
-                        <select className="register__select" name="area_id" id="area_select" onChange={handleChange}>
+                        <select className="register__select" name="area_id" id="area_id" onChange={handleChange}>
                             <option value="" className="register__option">Выбрать</option>
                             <option value="1" className="register__option">город Аргун</option>
                         </select>
-                        <span className="txt-danger" id="danger-position">Заполните поле </span>
+                        <span className="notif" id="danger-position"></span>
                     </div>
 
                     <button className="register__btn btn" type='submit' onClick={onClick}>Зарегистрироваться</button>
+
+                    <div className="message">
+                        {/* <h3 className="message__title">Спасибо!</h3> */}
+                        <p className="message__text">{messageNot}</p>
+                    </div>
                 </div>
 
 
-                {/* <div className="message">
-                    <h3 className="message__title">Спасибо!</h3>
-                    <p className="message__text">Вы успешно зарегистрированы на мероприятие</p>
-                </div> */}
+
+
             </div>
         </div>
     );
